edf25e15779a4e8f71972e1c40e93712caead4bb
Sajjad <sssanikhani@gmail.com>
2021-12-21 16:21:17 +0330

    feat: additional calcPrice method test for covering mutation (hit 90%)

--- src/test/java/org/springframework/samples/petclinic/utility/PriceCalculatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/utility/PriceCalculatorTest.java
@@ -31,7 +31,7 @@ public class PriceCalculatorTest {
 		when(v1.getDate()).thenReturn(LocalDate.of(2000, 1, 1));
 		when(v2.getDate()).thenReturn(LocalDate.of(LocalDate.now().getYear(), 1, 1));
 
-		when(p1.getBirthDate()).thenReturn(LocalDate.of(2000, 1, 1));
+		when(p1.getBirthDate()).thenReturn(LocalDate.of(2010, 1, 1));
 		when(p1.getVisitsUntilAge(anyInt())).thenReturn(new ArrayList<>());
 		when(p2.getBirthDate()).thenReturn(LocalDate.of(LocalDate.now().getYear(), 1, 1));
 		when(p2.getVisitsUntilAge(anyInt())).thenReturn(Arrays.asList(v1, v1));
@@ -51,73 +51,38 @@ public class PriceCalculatorTest {
 	}
 
 	@Test
-	public void calcPriceWithAllPetTypes() {
+	public void calcPriceWithAllPetTypesTest() {
 		List<Pet> pets = new ArrayList<>(Arrays.asList(p1, p2, p3));
 		assertEquals(8160.0, PriceCalculator.calcPrice(pets, 1000, 2000), 0);
 	}
 
 	@Test
-	public void calcPriceWithHighDiscount() {
+	public void calcPriceWithHighDiscountTest() {
 		List<Pet> pets = new ArrayList<>();
 		for (int i = 0; i < 6; i++)
 			pets.add(p2);
 		assertEquals(9.745544E7, PriceCalculator.calcPrice(pets, 1000, 2000), 0);
 	}
-}
 
-//public class CustomerDependentPriceCalculatorTest {
-//
-//	@Mock
-//	PetType rarePT, noRarePT;
-//
-//	LocalDate longBefore = new GregorianCalendar(2000, Calendar.JANUARY, 1).getTime();
-//	Date shortBefore = new GregorianCalendar(LocalDate.now().getYear(), Calendar.JANUARY, 1).getTime();
-//
-//	List<Pet> pets = new ArrayList<>(Arrays.asList(rareAgedPet, rareYoungPet, noRareAgedPet, noRareYoungPet));
-//
-//	CustomerDependentPriceCalculator cdp = new CustomerDependentPriceCalculator();
-//
-//	private List<Pet> buildHighDiscountPets() {
-//		List<Pet> temp = new ArrayList<>();
-//		for (int i = 0; i < 10; i++)
-//			temp.add(this.rareYoungPet);
-//		return temp;
-//	}
-//
-//	@Before
-//	public void setup() {
-//		when(this.rarePT.getRare()).thenReturn(true);
-//		when(this.noRarePT.getRare()).thenReturn(false);
-//		this.rareAgedPet.setType(this.rarePT);
-//		this.rareYoungPet.setType(this.rarePT);
-//		this.noRareAgedPet.setType(this.noRarePT);
-//		this.noRareYoungPet.setType(this.noRarePT);
-//		this.rareAgedPet.setBirthDate(this.longBefore);
-//		this.rareYoungPet.setBirthDate(this.shortBefore);
-//		this.noRareAgedPet.setBirthDate(this.longBefore);
-//		this.noRareYoungPet.setBirthDate(this.shortBefore);
-//	}
-//
-//	@Test
-//	public void calcPriceTestForVariationOfPets() {
-//		assertEquals(10160, cdp.calcPrice(this.pets, 1000, 2000, UserType.NEW), 0);
-//	}
-//
-//	@Test
-//	public void calcPriceTestForGoldUserAndLowDiscount() {
-//		assertEquals(9128, cdp.calcPrice(this.pets, 1000, 2000, UserType.GOLD), 0);
-//	}
-//
-//	@Test
-//	public void calcPriceTestForHighDiscountAndNewUser() {
-//		List<Pet> pl = this.buildHighDiscountPets();
-//		assertEquals(32920, cdp.calcPrice(pl, 1000, 2000, UserType.NEW), 0);
-//	}
-//
-//	@Test
-//	public void calcPriceTestForHighDiscountAndSilverUser() {
-//		List<Pet> pl = this.buildHighDiscountPets();
-//		assertEquals(31140, cdp.calcPrice(pl, 1000, 2000, UserType.SILVER), 0);
-//	}
-//
-//}
+	@Test
+	public void calcPriceWithP1Test() {
+		List<Pet> pets = new ArrayList<>();
+		for(int i = 0; i < 10; i++)
+			pets.add(p1);
+		assertEquals(46600.0, PriceCalculator.calcPrice(pets, 1000, 2000), 0);
+	}
+
+	@Test
+	public void calcPriceWithP3Test() {
+		List<Pet> pets = new ArrayList<>();
+		for(int i = 0; i < 10; i++)
+			pets.add(p3);
+		assertEquals(4590200.0, PriceCalculator.calcPrice(pets, 1000, 2000), 0);
+	}
+
+	@Test
+	public void calcPriceWithP1P3Test() {
+		List<Pet> pets = new ArrayList<>(Arrays.asList(p1, p3, p3, p1, p3, p1, p3, p1, p1, p3));
+		assertEquals(4590200.0, PriceCalculator.calcPrice(pets, 1000, 2000), 0);
+	}
+}
